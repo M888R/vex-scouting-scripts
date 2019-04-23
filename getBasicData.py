5487b8890f70a600f5fe5731a6713bf294300c5d
Manas Shah <manas@shahparivar.info>
2019-04-22 21:17:56 -0700

    team

--- getBasicData.py
+++ getBasicData.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 # can loop through some data file for which teams to search
-requested_team = "2150B"
+requested_team = "1104Z"
 
 get_rankings_url = "https://api.vexdb.io/v1/get_rankings?team=" + requested_team + "&season=Turning Point"
 get_matches_url = "https://api.vexdb.io/v1/get_matches?team=" + requested_team + "&season=Turning Point"
